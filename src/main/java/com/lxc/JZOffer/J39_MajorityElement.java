9c3b1f4513a00f074650b7e790b0ce2d17d86335
lxc <48900984+xc-lin@users.noreply.github.com>
2021-08-01 23:36:27 +0800

    add questions and answer

--- src/main/java/com/lxc/JZOffer/J39_MajorityElement.java
+++ src/main/java/com/lxc/JZOffer/J39_MajorityElement.java
@@ -1,4 +1,47 @@
 package com.lxc.JZOffer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class J39_MajorityElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                int a = map.get(num);
+                map.put(num, ++a);
+            } else {
+                map.put(num, 1);
+            }
+
+
+        }
+        int k = 0;
+        int result = 0;
+        for (int a : map.keySet()) {
+            if (result < map.get(a)) {
+                result = map.get(a);
+                k = a;
+            }
+        }
+        return k;
+    }
+
+    public int majorityElementBest(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        Map.Entry<Integer, Integer> result=null;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (result==null||entry.getValue()>result.getValue()){
+                result=entry;
+            }
+        }
+        return result.getKey();
+    }
 }
